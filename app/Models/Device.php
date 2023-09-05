01aae9f20ac15a0da780832603db5ee872a20082
Валерий Стельмашок <valera.stelmashock@yandex.ru>
2023-09-05 12:11:42 +0300

    add to project ide-helper

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -5,6 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Device
+ *
+ * @property int $id
+ * @property string|null $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Device newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Device newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Device query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Device whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Device whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Device whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Device whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Device extends Model
 {
     use HasFactory;
